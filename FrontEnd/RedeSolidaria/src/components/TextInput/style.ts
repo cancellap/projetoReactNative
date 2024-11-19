6f391694546ce23a7fba7d4cfd78cc7f49a64733
Rayssa <vieirarayssa167@gmail.com>
2024-11-19 20:13:00 -0300

    Pagina Login e TextInput

---
+++ FrontEnd/RedeSolidaria/src/components/TextInput/style.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  boxInput: {
+    backgroundColor: '#B4D4FF',
+    width: '90%',
+    margin: 5,
+    justifyContent: 'center'
+  },
+
+  input: {
+    margin: 5,
+    width: "70%",
+    height: 55,
+    borderRadius: 5,
+    paddingLeft: 20,
+    fontSize: 20,
+    paddingRight: 45,
+  },
+
+  boxIcon: {
+    position: 'absolute',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 0,
+    width:  80,
+  }
+});
