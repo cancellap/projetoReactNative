93436839c5cea64e8c74911cf2f06ba70560194b
CaiqueROC <caique_ramos2010@hotmail.com>
2024-11-19 10:45:27 -0300

    subindo pasta para api

---
+++ Rede-Solidaria/src/test/java/com/redesolidaria/Rede_Solidaria/RedeSolidariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redesolidaria.Rede_Solidaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeSolidariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
