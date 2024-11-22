fc6cd7d22926c95dab14a6ef9022758189c67538
MtAguiar <matheus.saguiar@hotmail.com>
2024-11-22 20:22:11 -0300

    Alteração tabela usuário

--- Rede-Solidaria/src/main/resources/db/migration/V01__cria_tabela_usuario.sql
+++ Rede-Solidaria/src/main/resources/db/migration/V01__cria_tabela_usuario.sql
@@ -3,5 +3,6 @@ CREATE TABLE usuario
     usuario_id    serial PRIMARY KEY,
     nome_usuario  VARCHAR(50),
     email_usuario VARCHAR(255),
-    senha_usuario VARCHAR(255)
+    senha_usuario VARCHAR(255),
+    role VARCHAR(255)
 )
