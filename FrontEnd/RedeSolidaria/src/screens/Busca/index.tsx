f12de7790f8e8dd9162cdaf864e4850e83bb0b3f
CaiqueROC <caique_ramos2010@hotmail.com>
2024-11-23 13:04:50 -0300

    alteração ip

--- FrontEnd/RedeSolidaria/src/screens/Busca/index.tsx
+++ FrontEnd/RedeSolidaria/src/screens/Busca/index.tsx
@@ -27,7 +27,7 @@ export const Busca = () => {
 
   const getHome = async () => {
     try {
-      const url = `http://192.168.1.12:8080/instituicao`;
+      const url = `http://192.168.0.108:8080/instituicao`;
       const result = await axios.get(url, {
         headers: {
           Authorization: token,
--- FrontEnd/RedeSolidaria/src/screens/Cadastro/index.tsx
+++ FrontEnd/RedeSolidaria/src/screens/Cadastro/index.tsx
@@ -27,7 +27,7 @@ export const Cadastro = () => {
 
     try {
       const response = await axios.post(
-        "http://192.168.1.12:8080/usuarios",
+        "http://192.168.0.108:8080/usuarios",
 
         data
       );
